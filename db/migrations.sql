5a796757e9897ae4bb6f3ead0b3d4396922cc607
Noemi Romero <nlll02@yahoo.com>
2018-05-14 11:46:45 -0500

    Updated database and tables

--- db/migrations.sql
+++ db/migrations.sql
@@ -11,13 +11,11 @@ CREATE TABLE players(
 	username VARCHAR(256),
 	password_digest VARCHAR(256),
 	name VARCHAR(256),
-	location VARCHAR(256),
-	availability DATETIME,
-	notes VARCHAR(256),
 	pos VARCHAR(3),
 	email VARCHAR(256),
 	phone VARCHAR(15),
-	# TEAM ID?
+	game_id INT REFERENCES games(id)
+
 );
 
 CREATE TABLE teams(
@@ -25,20 +23,23 @@ CREATE TABLE teams(
 	username VARCHAR(256),
 	password_digest VARCHAR(256),
 	name VARCHAR(256),
-	location VARCHAR(256),
-	player_id INT REFERENCES players(id),
-	game_id INT REFERENCES games(id),
-);
-
-CREATE TABLE enrollments(
-	id SERIAL PRIMARY KEY,
-	player_id INT REFERENCES players(id),
-	team_id INT REFERENCES teams(id)
+	
+	
 );
 
 CREATE TABLE games(
 	id SERIAL PRIMARY KEY,
-	gameinfo DATETIME,
-	team_id INT REFERENCES teams(id),
+	date VARCHAR(256),
+	time VARCHAR(256),
+	team_id INT REFERENCES teams(id)
+	
 );
+
+
+CREATE TABLE availability(
+	id SERIAL PRIMARY KEY,
+	player_id INT REFERENCES players(id),
+	game_id INT REFERENCES games(id),
+	available BOOLEAN
+)
 
